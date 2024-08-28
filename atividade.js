90d98f6563bd7634aeac106b1f9d09ae98ebbd7a
joaodeco <murilomergner@gmail.com>
2024-08-28 15:29:40 -0300

    Código 3.0 Tratamento de erro

--- atividade.js
+++ atividade.js
@@ -10,6 +10,10 @@ app.get('/info', (req, res)  => {
     res.send('Essa API foi feita pelo Deco 🤠')
 })
 
+app.get('/erro', (req, res)  => {
+    res.status(404).send('Essa pagina não existe 😔')
+})
+
 app.listen(porta, () => {
     console.log('Escutando em http://localhost:3000 !')
 })
